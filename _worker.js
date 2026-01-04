4fa21ea968cbd6c67540a165b5866b76c0ba05f2
aresbigboy <wei.ares@gmail.com>
2026-01-04 15:33:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -122,7 +122,7 @@ function error(...args) {
 }
 
 function isValidUserId(uuid) {
-    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
     return uuidRegex.test(uuid);
 }
 
