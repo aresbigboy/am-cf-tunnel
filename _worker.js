8a14497f0e8be2496a4b899ba37aec42d93018f5
aresbigboy <wei.ares@gmail.com>
2026-01-06 17:08:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ let pDomain = [];
 
 let p64 = true;
 let p64DnUrl = atob('aHR0cHM6Ly8xLjEuMS4xL2Rucy1xdWVyeQ==');
-let p64Prefix = atob('MjAwMTo2N2M6MmIwOmRiMzI6Og==');
+let p64Prefix = atob('MmEwMTo0Zjk6YzAxMDozZjAyOjY0Ojo=');
 let p64Domain = [];
 
 let s5 = '';
